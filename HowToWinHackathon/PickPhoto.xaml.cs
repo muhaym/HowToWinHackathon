8f02381ec6eb5c4d090869baa192c4d65c79f789
Abdul Muhaymin <mampary@hotmail.com>
2017-10-05 02:17:01 +0530

    Updated Packages

---
+++ HowToWinHackathon/PickPhoto.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Plugin.Media;
+
+using Xamarin.Forms;
+
+namespace HowToWinHackathon
+{
+    public partial class PickPhoto : ContentPage
+    {
+        public PickPhoto()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
